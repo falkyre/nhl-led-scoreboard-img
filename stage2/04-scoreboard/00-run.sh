005f389290c43363111c0e276558cdd9ae22226f
Sean Ostermann <falkyre@gmail.com>
2021-01-18 19:35:14 +0000

    Initial successful first image build

--- stage2/04-scoreboard/00-run.sh
+++ stage2/04-scoreboard/00-run.sh
@@ -7,13 +7,14 @@ pip3 install --upgrade pip
 pip3 install requests
 pip3 install regex
 pip3 install geocoder python_tsl2591 ephem
-pip3 uninstall numpy
+pip3 uninstall -y numpy
 pip3 install env-canada==0.0.35
 pip3 install --upgrade pyowm
 pip3 install noaa_sdk fastjsonschema
 pip3 install apscheduler
 pip3 install lastversion
 pip3 install numpy
+pip3 install supervisor
 
 #Clone scoreboard repo
 cd /home/pi
@@ -27,10 +28,10 @@ git config submodule.matrix.ignore all
 
 cd submodules/matrix 
 
-make build-python PYTHON=$(which python3)
-sudo make install-python PYTHON=$(which python3)
-cd bindings
-pip3 install -e python/
+make build-python PYTHON=/usr/bin/python3
+make install-python PYTHON=/usr/bin/python3
+#cd bindings
+#pip3 install -e python/
 
 cd /home/pi
 chown -R pi:pi nhl-led-scoreboard
