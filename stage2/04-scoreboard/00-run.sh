a2e0e5796f99ee4b271e6edb65dc616d4a118ed4
Sean Ostermann <falkyre@gmail.com>
2021-01-20 16:51:19 +0000

    Install the python requirements for scoreboard from the requirements.txt file.  Move install of supervisor into supervisor stage

--- stage2/04-scoreboard/00-run.sh
+++ stage2/04-scoreboard/00-run.sh
@@ -4,33 +4,38 @@ on_chroot << EOF
 #Remove packages that might impact performance as per https://github.com/hzeller/rpi-rgb-led-matrix
 apt-get -y remove bluez bluez-firmware pi-bluetooth triggerhappy
 
-#Install pip requirements for scoreboard
+#Install pip requirements for scoreboard from the requirements.txt
 
 pip3 install --upgrade pip
-pip3 install requests
-pip3 install regex
-pip3 install geocoder python_tsl2591 ephem
-pip3 uninstall -y numpy
-pip3 install env-canada==0.0.35
-pip3 install --upgrade pyowm
-pip3 install noaa_sdk fastjsonschema
-pip3 install apscheduler
-pip3 install lastversion
-pip3 install numpy
-pip3 install supervisor
+#pip3 install requests
+#pip3 install regex
+#pip3 install geocoder python_tsl2591 ephem
+#pip3 uninstall -y numpy
+#pip3 install env-canada==0.0.35
+#pip3 install --upgrade pyowm
+#pip3 install noaa_sdk fastjsonschema
+#pip3 install apscheduler
+#pip3 install lastversion
+#pip3 install numpy
+#pip3 install namespace
 
 # Install pip requirements for nhl_setup
-pip3 install fastjsonschema
-pip3 install printtools
-pip3 install PyInstaller
-pip3 install questionary
-pip3 install regex
+#pip3 install fastjsonschema
+#pip3 install printtools
+#pip3 install PyInstaller
+#pip3 install questionary
+#pip3 install regex
+
 
 #Clone scoreboard repo
 cd /home/pi
 git clone https://github.com/riffnshred/nhl-led-scoreboard.git
 
 cd nhl-led-scoreboard
+
+#Install the python requirements from the requiremenst.txt file
+pip3 install -r requirements.txt 
+
 #Install rgb matrix
 # Pull submodule and ignore changes from script
 git submodule update --init --recursive
--- stage2/06-supervisor/00-run.sh
+++ stage2/06-supervisor/00-run.sh
@@ -9,6 +9,9 @@ install -v -m 644 files/scoreboard.conf ${ROOTFS_DIR}/etc/supervisor/conf.d
 install -v -m 644 files/supervisord.service                 "${ROOTFS_DIR}/etc/systemd/system/"
 
 on_chroot << EOF
+#Install supervisor
+pip3 install supervisor
+
 #Create systemd
 systemctl unmask supervisord
 systemctl enable supervisord 
