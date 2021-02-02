be8e226c655e59d7da9576050a9d5a05e9ef80d1
Sean Ostermann <falkyre@gmail.com>
2021-02-02 18:24:39 +0000

    tweaks to neofetch config and initial system wide bashrc

--- stage2/04-scoreboard/files/scoreboard.bash
+++ stage2/04-scoreboard/files/scoreboard.bash
@@ -1,3 +1,19 @@
+# Aliases
+# - sudo alias that allows running other aliases with "sudo": https://github.com/MichaIng/DietPi/issues/424
+alias sudo='sudo '
+# - Scoreboard programs
+alias sb-issue='/home/pi/sbtools/issueUpload.sh 2>1'
+alias sb-check='/home/pi/sbtools/checkUpdate.sh'
+alias sb-stderr='supervisorctl tail -50000 scoreboard stderr'
+alias sb-stdout='supervisorctl tail -50000 scoreboard'
+alias sb-livelog='supervisorctl tail -f scoreboard'
+alias sb-stop='supervisorctl stop scoreboard'
+alias sb-start='supvisorctl start scoreboard'
+alias sb-restart='supervisorctl restart scoreboard'
+alias sb-status='supervisorctl status scoreboard'
+alias sb-tools='/home/pi/sbtools/sb-tools'
+alias sb-sysinfo='neofetch --off'
+
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
