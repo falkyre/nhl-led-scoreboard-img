7bc990fd55e8043fa8b5e5e077e0da830afc8b1a
Sean Ostermann <falkyre@gmail.com>
2021-02-03 21:41:33 +0000

    more tweaks to system wide bashrc and sb-tools

--- stage2/04-scoreboard/files/scoreboard.bash
+++ stage2/04-scoreboard/files/scoreboard.bash
@@ -2,8 +2,9 @@
 # - sudo alias that allows running other aliases with "sudo": https://github.com/MichaIng/DietPi/issues/424
 alias sudo='sudo '
 # - Scoreboard programs
+alias sb-help='echo "$(tput smul)scoreboard commands:$(tput rmul) $(tput bold)sb-tools sb-issue sb-updatecheck sb-stderr sb-stdout sb-livelog sb-stop sb-start sb-restart sb-status sb-sysinfo$(tput sgr0)"'
 alias sb-issue='/home/pi/sbtools/issueUpload.sh 2>1'
-alias sb-check='/home/pi/sbtools/checkUpdate.sh'
+alias sb-updatecheck='/home/pi/sbtools/checkUpdate.sh'
 alias sb-stderr='supervisorctl tail -50000 scoreboard stderr'
 alias sb-stdout='supervisorctl tail -50000 scoreboard'
 alias sb-livelog='supervisorctl tail -f scoreboard'
@@ -20,5 +21,8 @@ parse_git_branch() {
 
 export PS1="\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ "
 
-#Output sysinfo
-neofetch --off
+#Output sysinfo only if not the root user
+
+if [ $USER != "root" ]; then
+   neofetch --off
+fi
