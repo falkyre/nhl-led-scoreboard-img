5640613b7e46114e98e6b47b6d84c74200c5341c
Sean Ostermann <falkyre@gmail.com>
2026-02-13 09:42:24 -0600

    feat: Implement pip package caching across build stages and GitHub Actions for faster image generation.

--- pi-gen-config/stage-nhl/prerun.sh
+++ pi-gen-config/stage-nhl/prerun.sh
@@ -1,7 +1,16 @@
-#!/bin/bash -e
-
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	echo "Manually copying rootfs from stage2..."
     mkdir -p "${ROOTFS_DIR}"
 	rsync -aHAX "${WORK_DIR}/stage2/rootfs/" "${ROOTFS_DIR}/"
+fi
+
+if [ -d "/pip-cache" ]; then
+    echo "Mounting /pip-cache to ${ROOTFS_DIR}/tmp/pip_cache"
+    mkdir -p "${ROOTFS_DIR}/tmp/pip_cache"
+    mount --bind /pip-cache "${ROOTFS_DIR}/tmp/pip_cache"
+    
+    # Ensure it gets unmounted on exit
+    # trap "umount ${ROOTFS_DIR}/tmp/pip_cache" EXIT
+else
+    echo "Warning: /pip-cache not found, skipping mount."
 fi
